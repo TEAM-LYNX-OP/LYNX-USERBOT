b2df5a40a07b4ff8a2cdd4e2cfdb9cac49f9e7be
Itz-UNKOWN-xd <93468291+Itz-UNKOWN-xd@users.noreply.github.com>
2022-01-31 11:26:21 +0530

    Update fpost.py

--- userbot/plugins/fpost.py
+++ userbot/plugins/fpost.py
@@ -29,14 +29,14 @@ async def all_groups_id(mafia):
 async def _(event):
     if event.fwd_from:
         return
-    if Config.MAFIABOT_LOGGER is None:
+    if Config.LYNXBOT_LOGGER is None:
         await edit_or_reply(
             event,
-            "Please set the required environment variable `MAFIABOT_LOGGER` for this plugin to work",
+            "Please set the required environment variable `LYNXBOT_LOGGER` for this plugin to work",
         )
         return
     try:
-        e = await event.client.get_entity(Config.MAFIABOT_LOGGER)
+        e = await event.client.get_entity(Config.LYNXBOT_LOGGER)
     except Exception as e:
         await edit_or_reply(event, str(e))
     else:
