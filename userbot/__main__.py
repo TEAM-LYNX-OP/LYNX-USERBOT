fdc40196ed5eb67b2549a3956314486852fc1bd5
Itz-UNKOWN-xd <93468291+Itz-UNKOWN-xd@users.noreply.github.com>
2022-01-31 10:41:14 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -12,7 +12,7 @@ from pathlib import Path
 import asyncio
 import telethon.utils
 
-MAFIA_PIC = Config.ALIVE_PIC or "https://telegra.ph/file/e97d640332ce5eadb3f89.mp4"
+Lynx_pic = Config.ALIVE_PIC or "https://te.legra.ph/file/05db5b237fb0a98d577bc.jpg"
 
 os.system("pip install -U telethon")
 
@@ -54,14 +54,14 @@ for name in files:
 
 import userbot._core
 
-print(f"""MAFIABOT IS ON!!! MAFIABOT VERSION :- {mafiaversion} YOUR 𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋 IS READY TO USE! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.alive/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @MafiaBot_Support .""")
+print(f"""LynxBot IS ON!!! LynxBot VERSION :- {mafiaversion} YOUR 𝑳𝒀𝑵𝑿 𝑩𝑶𝑻 IS READY TO USE! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.alive/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @LynxBot_Support .""")
 async def mafia_is_on():
     try:
         if Config.MAFIABOT_LOGGER != 0:
             await bot.send_file(
                 Config.MAFIABOT_LOGGER,
-                MAFIA_PIC,
-                caption=f"༆ʟɛɢɛռɖaʀʏ ᴀғ ᴍᴀғɪᴀʙᴏᴛ༆\n\n**𝚅𝙴𝚁𝚂𝙸𝙾𝙽 ➪ {mafiaversion}**\n\n𝐓𝐲𝐩𝐞 `.ping` or `.alive` 𝐭𝐨 𝐜𝐡𝐞𝐜𝐤! \n\n𝙹𝙾𝙸𝙽 [𝙼𝙰𝙵𝙸𝙰𝙱𝙾𝚃 𝙲𝙷𝙰𝚃](t.me/MAFIABOT_CHIT_CHAT) 𝚃𝙾 𝚀𝚄𝙴𝚁𝚈 & 𝙹𝙾𝙸𝙽 [𝙼𝙰𝙵𝙸𝙰 𝚄𝙿𝙳𝙰𝚃𝙴𝚂](t.me/MafiaBot_Support) 𝚃𝙾 𝙺𝙽𝙾𝚆 𝚁𝙴𝙶𝚁𝙰𝙳𝙸𝙽𝙶 𝚄𝙿𝙳𝙰𝚃𝙴 𝙰𝙽𝙳 𝙽𝙴𝚆𝚂 𝙰𝙱𝙾𝚄𝚃 𝙼𝙰𝙵𝙸𝙰𝙱𝙾𝚃",
+                Lynx_pic,
+                caption=f"༆𝓛Ⲉ𝓖ⲈⲚⲆⲀꞄⲨ Ⲁ𝓕 𝓛ⲨⲚⲬ ⲂⲞⲦ༆\n\n**𝚅𝙴𝚁𝚂𝙸𝙾𝙽 ➪ {mafiaversion}**\n\n𝐓𝐲𝐩𝐞 `.ping` or `.alive` 𝐭𝐨 𝐜𝐡𝐞𝐜𝐤! \n\n𝙹𝙾𝙸𝙽 [lynxBot 𝙲𝙷𝙰𝚃](t.me/LynxBot_Support) 𝚃𝙾 𝚀𝚄𝙴𝚁𝚈 & 𝙹𝙾𝙸𝙽 [LYNX 𝚄𝙿𝙳𝙰𝚃𝙴𝚂](t.me/lynx_updates) 𝚃𝙾 𝙺𝙽𝙾𝚆 𝚁𝙴𝙶𝚁𝙰𝙳𝙸𝙽𝙶 𝚄𝙿𝙳𝙰𝚃𝙴 𝙰𝙽𝙳 𝙽𝙴𝚆𝚂 𝙰𝙱𝙾𝚄𝚃 LynxBot",
             )
     except Exception as e:
         LOGS.info(str(e))
