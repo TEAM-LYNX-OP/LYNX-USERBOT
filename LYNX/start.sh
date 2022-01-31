6231a195f490c462dd37a1ac75795daf026dc2c3
Itz-UNKOWN-xd <93468291+Itz-UNKOWN-xd@users.noreply.github.com>
2022-01-31 10:34:19 +0530

    Create start.sh

---
+++ LYNX/start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#   Copyright (C) 2021 Mafiabot
+#
+#   Poject VΛDΣЯ is free software: 
+#   you can redistribute it and/or modify it under the terms of the GNU General Public License 
+#   as published by the Free Software Foundation, either version 3 of the License, 
+#   or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, 
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#   See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along with this program. 
+#   If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+echo """
+
+            
+██╗░░░░░██╗░░░██╗███╗░░██╗██╗░░██╗
+██║░░░░░╚██╗░██╔╝████╗░██║╚██╗██╔╝
+██║░░░░░░╚████╔╝░██╔██╗██║░╚███╔╝░
+██║░░░░░░░╚██╔╝░░██║╚████║░██╔██╗░
+███████╗░░░██║░░░██║░╚███║██╔╝╚██╗
+╚══════╝░░░╚═╝░░░╚═╝░░╚══╝╚═╝░░╚═╝
+                       𝐋𝐄𝐆𝐄𝐍𝐃𝐀𝐑𝐘 𝐀𝐅 𝐋𝐘𝐍𝐗 𝐔𝐒𝐄𝐑𝐁𝐎𝐓 !!
+                                          @LYNXBOT_SUPPORT
+"""
+python3 -m userbot
