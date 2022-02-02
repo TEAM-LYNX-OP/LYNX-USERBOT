35d0998ba372d522b6cc1b325fd7806b1f0e53f1
Itz-UNKOWN-xd <93468291+Itz-UNKOWN-xd@users.noreply.github.com>
2022-02-02 16:54:19 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -37,9 +37,9 @@ else:
             api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
         print("Initialisation finished with no errors")
-        print("Starting MafiaBot")
+        print("Starting LynxBot")
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
-        print("MafiaBot Startup Completed")
+        print("LynxBot Startup Completed")
     else:
         bot.start()
 
